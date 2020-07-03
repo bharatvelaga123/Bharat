4aade2a016166185a082f7fb808a628dcaf7f029
bharatvelaga123 <67789490+bharatvelaga123@users.noreply.github.com>
2020-07-03 17:50:35 +0530

    Add files via upload

---
+++ prog1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Prog1
+{
+	public static void main(String args[])
+	{
+		System.out.println(args[0]+" Technologies "+args[1]);
+	}
+}
+
+
+
