c51da6201d6262be0894dd284e77decf53a6892b
bharatvelaga123 <67789490+bharatvelaga123@users.noreply.github.com>
2020-07-03 17:51:42 +0530

    Add files via upload

---
+++ prog3.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.*;
+class prog3
+{
+	public static void main(String args[])
+	{	
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int c=a+b;
+		System.out.println("The sum of "+a+" and "+b+" is "+c);
+	}
+}
+
+
